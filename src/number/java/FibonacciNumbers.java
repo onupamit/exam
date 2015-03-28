5e39a0aa7acb632da8006eae0fec704441029a21
m rahman <rahmanww@gmail.com>
2015-03-28 08:35:39 -0400

    first commit

---
+++ src/number/java/FibonacciNumbers.java
@@ -0,0 +1,14 @@
+package number.java;
+
+public class FibonacciNumbers {
+
+	public static void main(String[] args) {
+		
+		/* 
+		 * Fibonacci numbers are 0,1,1,2,3,5,8,13,21
+		 * Write java code to find fibonacci sequences up to 100 numbers. 
+		 * 
+		 */
+	}
+
+}
